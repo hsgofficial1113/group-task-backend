ab03cb8f101c3ebb2e115f3e223f247d18041836
Phillip Howard <philliphoward413@gmail.com>
2025-08-04 07:42:59 -0400

    updated .env

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use('/api/tasks', taskRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+   const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error('MongoDB error:', err));
